6555116765465d62af49864bd97c60ef31dbbed8
stanislau.zhvanski <stanislau.zhvanski@idfeurasia.pro>
2026-04-01 12:44:22 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "nudes-protector-back"
---
+++ src/main/kotlin/mm/nudesprotectorback/NudesProtectorBackApplication.kt
@@ -0,0 +1,11 @@
+package mm.nudesprotectorback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NudesProtectorBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<NudesProtectorBackApplication>(*args)
+}
